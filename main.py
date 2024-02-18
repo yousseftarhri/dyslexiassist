747e2002f0955030dcaf4c5e71a331c204490761
yousseftarhri <yousseftarhri@gmail.com>
2024-02-18 02:16:00 +0100

    prompt

--- main.py
+++ main.py
@@ -25,6 +25,8 @@ vectore_store = get_embd()
 def prompt_ai(context) :
     prompt_ai = f"""
     You role is to speak with someone who wanna learn english, by speaking, help the user to improve his speak skills. use the context delimited by double quotes to answer the user question
+    Make sure to correct any spelling or writing errors made by the user and provide them with feedback.
+
     ""context : {context}""
     
     """
@@ -68,15 +70,15 @@ def start(update, context):
     bot = context.bot
     chat_id = update.message.chat_id
     document = collection.find_one({'_id': chat_id})
-    data = {"_id": chat_id, "conversation" : [
-        {"role": "system", "content": prompt},
-    ]}
+    data = {"_id": chat_id, "conversation" : []}
+    welcome = """Hello, 🌟
+Step into a world of language mastery with CasablancaAI. Your AI Language Assistant is ready to embark on an engaging language learning journey with you. Whether you're here to improve your speaking, writing, or listening skills, CasablancaAI is your personalized language tutor."""
     if document :
-        bot.send_message(chat_id=update.message.chat_id, text="*Welcome again to the bot*",parse_mode= 'Markdown')
+        bot.send_message(chat_id=update.message.chat_id, text=welcome,parse_mode= 'Markdown')
 
     else:
         collection.insert_one(data)
-        bot.send_message(chat_id=update.message.chat_id, text="*Welcome, you are new here*",parse_mode= 'Markdown')
+        bot.send_message(chat_id=update.message.chat_id, text=welcome,parse_mode= 'Markdown')
 
 
 
@@ -124,18 +126,43 @@ def voice_handler(update, context):
         print(f"An unexpected error occurred: {e}")
     print("ok")
 
+def text_handler(update, context):
+    bot = context.bot
+    chat_id = update.message.chat_id
+    try:
 
+        msg = update.message.text
+
+        existing_document = collection.find_one({"_id": chat_id})
+        existing_document['conversation'].append({ "role": "user", "content": msg })
+        docs = vectore_store.similarity_search(msg)
+        existing_document['conversation'][0] =  {"role": "system", "content": prompt_ai(docs)}
+
+        output_ai = response(existing_document['conversation'])
+        print(output_ai)
+        bot.send_message(chat_id=update.message.chat_id, text=output_ai)
+
+        existing_document['conversation'].append({ "role": "assistant", "content": output_ai })
+        collection.update_one({"_id": chat_id}, {'$set': {'conversation': existing_document['conversation']}})
+
+
+    except Exception as e:
+        bot = context.bot
+        chat_id = update.message.chat_id
+        bot.send_message(chat_id=chat_id, text='We have a problem in our bot we gonna fix it ASAP')
+        print(f"An unexpected error occurred: {e}")
+    print("ok")
 def main():
     """Start the bot."""
 
     updater = Updater(bot_token)
     dp = updater.dispatcher
-    # dp = ApplicationBuilder().token(bot_token).build()
 
     dp.add_handler(CommandHandler("start", start))
 
     dp.add_handler(MessageHandler(Filters.voice, voice_handler))
     dp.add_handler(MessageHandler(Filters.audio, voice_handler))
+    dp.add_handler(MessageHandler(Filters.text, text_handler))
 
     # log all errors
     dp.add_error_handler(error)
